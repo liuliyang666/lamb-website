a97443b47b1748a741293527b8f507776b769f4a
lilyyang <1592218619@qq.com>
2023-05-15 10:51:05 +0800

    update

---
+++ assets/TagCreate.9b466918.js
@@ -0,0 +1,1 @@
+import{B as t,M as a}from"./index.871f8bf3.js";import{T as e}from"./TagForm.988e71a2.js";import{bE as n,bF as o}from"./vendor.c585d91c.js";import"./vant.d5548487.js";const i=n({setup:(r,l)=>()=>o(a,null,{title:()=>"\u65B0\u5EFA\u6807\u7B7E",icon:()=>o(t,null,null),default:()=>o(e,null,null)})});export{i as TagCreate,i as default};
---
+++ assets/TagPage.b791dd43.js
@@ -0,0 +1,1 @@
+import{bE as e,bF as t,bV as o}from"./vendor.c585d91c.js";const p=e({props:{name:{type:String}},setup:(n,r)=>()=>t(o,null,null)});export{p as TagPage,p as default};
